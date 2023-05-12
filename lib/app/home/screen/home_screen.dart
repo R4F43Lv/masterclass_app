811cc6773b387987d48224e452dae184d7a52660
R4F43Lv <rafa.vini2000@outlook.com>
2023-05-12 07:51:14 -0300

    ajustes na bottombar

--- lib/app/home/screen/home_screen.dart
+++ lib/app/home/screen/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:app_masterclass/app/shared/theme/theme.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -31,7 +31,9 @@ class _HomeScreenState extends State<HomeScreen> {
                       'lib/assets/images/logo.png',
                       height: 48,
                     ),
-                    const SizedBox(width: 10,),
+                    const SizedBox(
+                      width: 10,
+                    ),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -40,20 +42,18 @@ class _HomeScreenState extends State<HomeScreen> {
                           'Atividades',
                           style: GoogleFonts.poppins(
                             textStyle: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                              color: Theme.of(context).highlightColor
-                            ),
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                                color: Theme.of(context).highlightColor),
                           ),
                         ),
                         Text(
                           'Flutterando MasterClass',
                           style: GoogleFonts.poppins(
                             textStyle: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w300,
-                              color: Theme.of(context).highlightColor
-                            ),
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300,
+                                color: Theme.of(context).highlightColor),
                           ),
                         ),
                       ],
@@ -63,7 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 InkWell(
                   onTap: () {
                     changeTheme.notifyTheme();
-                  } ,
+                  },
                   child: Image.asset(
                     'lib/assets/icons/moon.png',
                     color: Theme.of(context).highlightColor,
@@ -81,10 +81,26 @@ class _HomeScreenState extends State<HomeScreen> {
         type: BottomNavigationBarType.fixed,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         fixedColor: Theme.of(context).highlightColor,
-        unselectedLabelStyle: TextStyle(),
+        unselectedLabelStyle: GoogleFonts.poppins(
+          textStyle: TextStyle(
+            fontSize: 11,
+            color: Theme.of(context).highlightColor,
+          ),
+        ),
+        selectedLabelStyle: GoogleFonts.poppins(
+          textStyle: TextStyle(
+            fontSize: 11,
+            color: Theme.of(context).highlightColor,
+          ),
+        ),
+        elevation: 0,
         items: [
           BottomNavigationBarItem(
-            icon: Image.asset('lib/assets/icons/eather_target.png', height: 24, color: Theme.of(context).highlightColor,),
+            icon: Image.asset(
+              'lib/assets/icons/eather_target.png',
+              height: 24,
+              color: Theme.of(context).highlightColor,
+            ),
             label: 'Atividades',
           ),
           BottomNavigationBarItem(
@@ -96,7 +112,8 @@ class _HomeScreenState extends State<HomeScreen> {
             label: 'Repositorios',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person, size: 24, color: Theme.of(context).highlightColor),
+            icon: Icon(Icons.person,
+                size: 24, color: Theme.of(context).highlightColor),
             label: 'Sobre o Dev',
           ),
         ],
